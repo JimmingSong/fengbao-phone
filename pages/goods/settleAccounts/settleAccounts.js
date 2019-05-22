01f1064b96b4d3b06858c8080884cba7c713d6ff
jimmy <784985660@qq.com>
2019-05-22 23:48:59 +0800

    settleAccount

--- pages/goods/settleAccounts/settleAccounts.js
+++ pages/goods/settleAccounts/settleAccounts.js
@@ -6,14 +6,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    idList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let idList = JSON.parse(wx.getStorageSync('carArray'));
+    this.setData({
+      idList,
+    })
   },
 
   /**
