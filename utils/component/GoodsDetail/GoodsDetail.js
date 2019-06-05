d14638d7ab842980fe66f560683267be91ae0ce9
zawb-songjimin <jimin.song@zatech.com>
2019-06-05 21:15:01 +0800

    goodsDetail component

---
+++ utils/component/GoodsDetail/GoodsDetail.js
@@ -0,0 +1,28 @@
+// utils/component/GoodsDetail/GoodsDetail.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    goodsData:{
+      type: Object
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+  created() {
+    console.log(this)
+  }
+})
